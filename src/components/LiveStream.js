6fc83df3813a5f130733e3f6d364b8e541f994be
xavierdc-warnermedia <133021786+xavierdc-warnermedia@users.noreply.github.com>
2023-10-18 16:41:34 -0400

    added singular live output stream to iframe

--- src/components/LiveStream.js
+++ src/components/LiveStream.js
@@ -2,10 +2,11 @@ import React from "react";
 import './livestream.css';
 
 const LiveStream = () => {
+  const address = 'https://app.singular.live/output/3GF39ozicKphl7iXF57QkC/Output?aspect=16:9';
 
   return (
     <div className='live-stream'>
-      <iframe title='live' src=''>
+      <iframe title='live' src={address}>
       </iframe>
         Live Stream
     </div>
