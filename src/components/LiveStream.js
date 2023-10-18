30bc973a936b6f07b709ad551b1a043d91e34699
xavierdc-warnermedia <133021786+xavierdc-warnermedia@users.noreply.github.com>
2023-10-18 13:42:54 -0400

    added basic styling and iframe to host livestream

---
+++ src/components/LiveStream.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './livestream.css';
+
+const LiveStream = () => {
+
+  return (
+    <div className='live-stream'>
+      <iframe title='live' src=''>
+      </iframe>
+        Live Stream
+    </div>
+  )
+};
+
+export default LiveStream;
